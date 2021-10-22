ed6f127130fc6f81caebba9503777f9fd18c9e37
777shetty <shetty.chetu7@gmail.com>
2021-10-23 00:16:34 +0530

    ADDED:Server url to the site properties

--- Js/site_properties.js
+++ Js/site_properties.js
@@ -1,5 +1,6 @@
 let site_properties = {
     use_local_storage: "true",
+    server_url: "http://localhost:3000/ContactDB/",
     home_page: "../Pages/AddHome.html",
     add_contact_page:"../Pages/AddForm.html",
 } 
